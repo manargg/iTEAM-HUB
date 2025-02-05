36b833309eb41a17e66be8fe3ebe638f2a152dc0
manar mustafa <abuhaitmanar@gmail.com>
2025-02-05 22:20:07 +0300

    edit and left side nav added

---
+++ JS-files/main.js
@@ -0,0 +1,20 @@
+let leftsideNav = document.getElementById("leftside-nav");
+let listHum = document.getElementById("menu-purger");
+let closeIcon = document.querySelector("#leftside-nav .logo i");
+
+function menu(){
+    leftsideNav.classList.add("active");
+}
+
+function closeMenu(){
+    leftsideNav.classList.remove("active");
+}
+
+listHum.addEventListener("click", menu);
+closeIcon.addEventListener("click", closeMenu);
+
+document.querySelectorAll("#leftside-nav").forEach(link => {
+    link.addEventListener("click", () => {
+        leftsideNav.classList.remove("active");
+    })
+})
